bfe7cde700077d61b80e4f046def5603964a800f
X_AJ_X <88792057+XAJX179@users.noreply.github.com>
2024-12-19 17:48:50 +0530

    Feat : create hash function & buckets

--- lib/hashmap.rb
+++ lib/hashmap.rb
@@ -8,5 +8,16 @@ class HashMap
   def initialize
     @capacity = 16
     @load_factor = 0.75
+    @buckets = Array.new(@capacity)
+  end
+
+  # generates hash codes for key
+  def hash(key)
+    hash_code = 0
+    prime_number = 31
+
+    key.each_char { |char| hash_code = (prime_number * hash_code) + char.ord }
+
+    hash_code
   end
 end
